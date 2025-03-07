0744c3239476850eca8ad9bfddb9b5d54edb14e3
Ramapriya2345 <ramapavy@gmail.com>
2025-03-08 02:42:15 +0530

    removed api key

--- Equators/nlp_gcommand.py
+++ Equators/nlp_gcommand.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 CORS(app)  # Enable CORS for frontend communication
 
 # Set your Gemini API key
-genai.configure(api_key="AIzaSyBUmapZZX9f5dC0darbYQUrHWra7lUJ_oM")
+genai.configure(api_key="")
 
 # Suppress Google Cloud SDK warnings
 os.environ["CLOUDSDK_CORE_DISABLE_PROMPTS"] = "1"
